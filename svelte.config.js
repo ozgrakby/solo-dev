1355405484edf16345d6e73590b7c4bb312bfc70
ozgrakby <akbayozgur2@gmail.com>
2026-07-06 00:07:06 +0300

    FIX: deploy düzeltildi

--- svelte.config.js
+++ svelte.config.js
@@ -4,18 +4,21 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
+	// Svelte 5 runes modunu buraya taşıdık:
+	compilerOptions: {
+		runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+	},
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html', // SPA ve Supabase Auth için ŞART!
+			fallback: '404.html',
 			precompress: false,
 			strict: true
 		}),
 		paths: {
-            // DİKKAT: GitHub Repo adın neyse buraya onu yaz. 
-            // Örneğin reponun adı "solo-panel" ise '/solo-panel' yazmalısın.
-			base: process.argv.includes('dev') ? '' : '/repo-adini-buraya-yaz'
+			// GitHub Repo adın neyse buraya onu yaz (Örn: '/solo-dev')
+			base: process.argv.includes('dev') ? '' : '/solo-dev'
 		}
 	}
 };
